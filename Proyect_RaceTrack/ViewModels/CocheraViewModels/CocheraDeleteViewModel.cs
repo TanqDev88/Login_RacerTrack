231dfd4e05acf9ba6a7ac75d9d7a575ae1422e2a
tanqueta88 <oliverprim783@gmail.com>
2024-02-04 23:18:09 -0300

    The viewModel for the CocheraDelete model is modified

--- Proyect_RaceTrack/ViewModels/CocheraViewModels/CocheraDeleteViewModel.cs
+++ Proyect_RaceTrack/ViewModels/CocheraViewModels/CocheraDeleteViewModel.cs
@@ -1,18 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyect_RaceTrack.ViewModels.CocheraViewModels
 {
     public class CocheraDeleteViewModel
     {
         public int CocheraId { get; set; }
+        [Display(Name = "Nombre")]
         public string? CocheraNombre { get; set; }
+        [Display(Name = "Numero")]
         public int CocheraNumero { get; set; }
+        [Display(Name = "Sector")]
         public CocheraType CocheraSector { get; set; }
+        [Display(Name = "Mantenimiento")]
         public bool CocheraAptoMantenimiento { get; set; } = true;
+        [Display(Name = "Oficinas")]
         public bool CocheraOficinas { get; set; } = true;
-        //public virtual List<Pista> Pistas { get; set; }
+
     }
 }
