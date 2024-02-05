0582f5dbcde1ce3c848effa69d10c3952b0c4e16
tanqueta88 <oliverprim783@gmail.com>
2024-02-04 23:16:34 -0300

    The viewModel for the CocheraCreate model is modified

--- Proyect_RaceTrack/ViewModels/CocheraViewModels/CocheraCreateViewModel.cs
+++ Proyect_RaceTrack/ViewModels/CocheraViewModels/CocheraCreateViewModel.cs
@@ -1,16 +1,34 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyect_RaceTrack.ViewModels.CocheraViewModels
 {
     public class CocheraCreateViewModel
     {
         public int CocheraId { get; set; }
+
+        [Display(Name = "Nombre de la Cochera")]
+        [Required(ErrorMessage = "Debe ingresar el nombre de la cochera")]
         public string? CocheraNombre { get; set; }
+
+        [Display(Name = "Numero de Cochera")]
+        [Required(ErrorMessage = "El numero de cochera es obligatorio")]
         public int CocheraNumero { get; set; }
+
+        [Display(Name = "Sector")]
         public CocheraType CocheraSector { get; set; }
+
+        [Display(Name = "Cuenta con taller?")]
+        public bool CocheraAptoMantenimiento { get; set; } = true;
+
+        [Display(Name = "Cuenta con oficinas?")]
+        public bool CocheraOficinas { get; set; } = true;
+
+        [Display(Name = "Pistas")]
+        [Required(ErrorMessage = "Debe seleccionar al menos una pista")]
+        public List<int>? PistaIds { get; set; }
+    }
+}
         public bool CocheraAptoMantenimiento { get; set; } = true;
         public bool CocheraOficinas { get; set; } = true;
         public List<int>? PistaIds { get; set; }
