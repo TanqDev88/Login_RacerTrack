345f651d1616b9595a1452f683f6f5f5aa052d1f
tanqueta88 <oliverprim783@gmail.com>
2024-02-06 17:14:46 -0300

    The viewModel for the CocheraIndex model is modified

--- Proyect_RaceTrack/ViewModels/CocheraViewModels/CocheraIndexViewModel.cs
+++ Proyect_RaceTrack/ViewModels/CocheraViewModels/CocheraIndexViewModel.cs
@@ -1,23 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Proyect_RaceTrack.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyect_RaceTrack.ViewModels.CocheraViewModels
 {
     public class CocheraIndexViewModel
     {
-
-        //public List<Cochera> cocheras { get; set; } = new List<Cochera>();
         public List<Cochera> cocheras { get; set; } = new List<Cochera>();
         public String? NameFilterCoc { get; set; }
         public int CocheraId { get; set; }
+        [Display(Name = "Nombre")]
         public string? CocheraNombre { get; set; }
+        [Display(Name = "Numero")]
         public int CocheraNumero { get; set; }
+        [Display(Name = "Sector")]
         public CocheraType CocheraSector { get; set; }
+        [Display(Name = "Mantenimiento")]
         public bool CocheraAptoMantenimiento { get; set; } = true;
+        [Display(Name = "Oficinas")]
         public bool CocheraOficinas { get; set; } = true;
-        //public virtual List<Pista> Pistas { get; set; }
+
     }
 }
