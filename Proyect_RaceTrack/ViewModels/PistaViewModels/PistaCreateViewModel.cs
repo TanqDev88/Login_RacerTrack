e509e4aa4fb16edfcb9046ab4fdfe77b40b3ec18
tanqueta88 <oliverprim783@gmail.com>
2024-02-09 01:56:08 -0300

    The viewModel for the PistaCreate is modified

--- Proyect_RaceTrack/ViewModels/PistaViewModels/PistaCreateViewModel.cs
+++ Proyect_RaceTrack/ViewModels/PistaViewModels/PistaCreateViewModel.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Proyect_RaceTrack.Models;
 using System.ComponentModel.DataAnnotations;
 using Proyect_RaceTrack.Utils;
 
@@ -12,11 +7,22 @@ namespace Proyect_RaceTrack.ViewModels.PistaViewModels
     {
 
         public int PistaId { get; set; }
+        [Display(Name = "Nombre de la pista")]
+        [Required(ErrorMessage = "Debe ingresar el nombre de la pista")]
         public string? PistaNombre { get; set; }
+        [Display(Name = "Nomenclatura")]
+        [Required(ErrorMessage = "Debe ingresar la nomenclatura de la pista")]
         public int PistaCodigo { get; set; }
+        [Display(Name = "Tipo")]
+        [Required(ErrorMessage = "Debe ingresar el tipo de material")]
         public PistaType PistaMaterial { get; set; }
+        [Display(Name = "Iluminacion")]
         public bool PistaIluminacion { get; set; } = true;
+        [Display(Name = "Aprovisionamiento")]
         public bool PistaAprovisionamiento { get; set; } = true;
-        public List<int> CocheraIds { get; set; }
+        
+        [Display(Name = "Cocheras")]
+        [Required(ErrorMessage = "Debes seleccionar al menos una cochera")]
+        public List<int>? CocheraIds { get; set; }
     }
 }
