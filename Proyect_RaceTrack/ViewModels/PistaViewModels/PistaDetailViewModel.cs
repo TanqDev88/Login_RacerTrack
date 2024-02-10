372f14c1dd2efc35d367264f4e800433d7f6b3ed
tanqueta88 <oliverprim783@gmail.com>
2024-02-10 03:30:26 -0300

    The viewModel for the PistaDetail is modified

--- Proyect_RaceTrack/ViewModels/PistaViewModels/PistaDetailViewModel.cs
+++ Proyect_RaceTrack/ViewModels/PistaViewModels/PistaDetailViewModel.cs
@@ -16,7 +16,32 @@ public class PistaDetailViewModel{
         [Display(Name = "Pista codigo")]
         public int PistaCodigo { get; set; }
         public PistaType PistaMaterial { get; set; }
+        [Display(Name = "Iluminacion")]using Proyect_RaceTrack.Models;
+using System.ComponentModel.DataAnnotations;
+using Proyect_RaceTrack.Utils;
+
+namespace Proyect_RaceTrack.ViewModels.PistaViewModels;
+
+public class PistaDetailViewModel{
+
+        public int PistaId { get; set; }
+        [Display(Name = "Nombre")]
+        public string? PistaNombre {get;set;}
+        [Display(Name = "Pista codigo")]
+        public int PistaCodigo { get; set; }
+
+        [Display(Name = "Material")]
+        public PistaType PistaMaterial { get; set; }
         [Display(Name = "Iluminacion")]
+        public bool PistaIluminacion {get;set;} = true;
+        [Display(Name = "Aprovisionamiento")]
+        public bool PistaAprovisionamiento {get;set;} = true;
+
+        public int CocheraId {get; set;}
+        public List<Cochera>? Cocheras {get;set;}
+
+}
+
         public bool PistaIluminacion {get;set;} = true;
         [Display(Name = "Aprovisionamiento")]
         public bool PistaAprovisionamiento {get;set;} = true;
