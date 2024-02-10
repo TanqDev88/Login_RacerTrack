3c968748f51052df51b1c8c8ecafc3c8598bab49
tanqueta88 <oliverprim783@gmail.com>
2024-02-10 03:32:15 -0300

    The viewModel for the PistaEdit is modified

--- Proyect_RaceTrack/ViewModels/PistaViewModels/PistaEditViewModel.cs
+++ Proyect_RaceTrack/ViewModels/PistaViewModels/PistaEditViewModel.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Proyect_RaceTrack.Models;
 using System.ComponentModel.DataAnnotations;
 using Proyect_RaceTrack.Utils;
 
@@ -12,21 +7,19 @@ namespace Proyect_RaceTrack.ViewModels.PistaViewModels
         public class PistaEditViewModel{
 
         public int PistaId { get; set; }
-        [Display(Name = "Nombre")]
-        public string? PistaNombre {get;set;}
-        [Display(Name = "Nomenclatura")]
+                [Display(Name = "Nombre de la pista")]
+                [Required(ErrorMessage = "Debe ingresar el nombre de la pista")]
+                public string? PistaNombre {get;set;}
+                [Display(Name = "Nomenclatura")]
+                [Required(ErrorMessage = "Debe ingresar la nomenclatura de la pista")]
                 public int PistaCodigo { get; set; }
                 public PistaType PistaMaterial { get; set; }
                 [Display(Name = "Iluminacion")]
-        public bool PistaIluminacion {get;set;} = true;
-        [Display(Name = "Aprovisionamiento")]
-        public bool PistaAprovisionamiento {get;set;} = true;
+        public bool PistaIluminacion { get; set; } = true;
+                [Display(Name = "Aprovisionamiento")]
+                public bool PistaAprovisionamiento { get; set; } = true;
 
-        //public List<int> HangarIds {get;set;}
+                public List<int>? CocheraIds { get; set; }
 
-        //public virtual List<Hangar> Hangars {get;set;}
-        //        [Display(Name = "Tipo de aeronave")]
-        // public virtual List<Hangar> Hangars {get;set;}    
-
-}
+        }
 }
