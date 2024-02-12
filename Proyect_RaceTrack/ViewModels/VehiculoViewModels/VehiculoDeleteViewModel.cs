bdd79d2aa928014d4d529113b380ee2f24c27f9a
tanqueta88 <oliverprim783@gmail.com>
2024-02-12 16:51:46 -0300

    The viewModel for the VehiculoDelete is modified

--- Proyect_RaceTrack/ViewModels/VehiculoViewModels/VehiculoDeleteViewModel.cs
+++ Proyect_RaceTrack/ViewModels/VehiculoViewModels/VehiculoDeleteViewModel.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Proyect_RaceTrack.Models;
+using Proyect_RaceTrack.Utils;
 
 namespace Proyect_RaceTrack.ViewModels.VehiculoViewModels
 {
@@ -26,16 +23,14 @@ namespace Proyect_RaceTrack.ViewModels.VehiculoViewModels
         [MaxLength(15)]
         public string? VehiculoApellido { get; set; }
 
-        [Display(Name = "Tipo de vehiculo")]
+        [Display(Name = "Tipo de Carroceria")]
         [Required(ErrorMessage = "Debe ingresar el tipo de vehiculo")]
         [MinLength(3, ErrorMessage = "El nombre ingresado debe poseer mas de tres letras")]
         [MaxLength(15)]
-        public string? VehiculoTipo { get; set; }
+        public VehiculoType VehiculoTipo { get; set; }
 
         [Display(Name = "Matricula")]
         [Required(ErrorMessage = "Debe ingresar el numero de matricula")]
-        // [MinLength(3, ErrorMessage = "El nombre ingresado debe poseer mas de tres letras")]  
-        // [MaxLength(15)] 
         [MinLength(6, ErrorMessage = "La matricula debe contener al menos 6 caracteres")]
         [MaxLength(7)]
         public string? VehiculoMatricula { get; set; }
