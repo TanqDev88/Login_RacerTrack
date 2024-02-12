2bc035d850d371405e62ca109223b5172c78cc13
tanqueta88 <oliverprim783@gmail.com>
2024-02-12 16:56:22 -0300

    The viewModel for the VehiculoEdit is modified

--- Proyect_RaceTrack/ViewModels/VehiculoViewModels/VehiculoEditViewModel.cs
+++ Proyect_RaceTrack/ViewModels/VehiculoViewModels/VehiculoEditViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Proyect_RaceTrack.Models;
+using Proyect_RaceTrack.Utils;
 
 namespace Proyect_RaceTrack.ViewModels.VehiculoViewModels
 {
@@ -11,9 +12,6 @@ namespace Proyect_RaceTrack.ViewModels.VehiculoViewModels
     {
 
         public int VehiculoId { get; set; }
-        //public List<Vehiculo> vehiculos { get; set; } = new List<Vehiculo>();
-
-        //public String? NameFilter { get; set; }
 
         [Display(Name = "Nombre Propietario")]
         [Required(ErrorMessage = "Debe ingresar el nombre del propietario")]
@@ -27,16 +25,13 @@ namespace Proyect_RaceTrack.ViewModels.VehiculoViewModels
         [MaxLength(15)]
         public string? VehiculoApellido { get; set; }
 
-        [Display(Name = "Tipo de vehiculo")]
+        [Display(Name = "Tipo de carroceria")]
         [Required(ErrorMessage = "Debe ingresar el tipo de vehiculo")]
-        [MinLength(3, ErrorMessage = "El nombre ingresado debe poseer mas de tres letras")]
-        [MaxLength(15)]
-        public string? VehiculoTipo { get; set; }
+        public VehiculoType VehiculoTipo { get; set; }
 
         [Display(Name = "Matricula")]
         [Required(ErrorMessage = "Debe ingresar el numero de matricula")]
-        // [MinLength(3, ErrorMessage = "El nombre ingresado debe poseer mas de tres letras")]  
-        // [MaxLength(15)] 
+
         [MinLength(6, ErrorMessage = "La matricula debe contener al menos 6 caracteres")]
         [MaxLength(7)]
         public string? VehiculoMatricula { get; set; }
