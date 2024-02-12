f30f977f288735c32f37d312d1887dd0eea1fde2
tanqueta88 <oliverprim783@gmail.com>
2024-02-12 16:57:23 -0300

    The viewModel for the VehiculoIndex is modified

--- Proyect_RaceTrack/ViewModels/VehiculoViewModels/VehiculoIndexViewModel.cs
+++ Proyect_RaceTrack/ViewModels/VehiculoViewModels/VehiculoIndexViewModel.cs
@@ -26,7 +26,7 @@ namespace Proyect_RaceTrack.ViewModels.VehiculoViewModels
         [MaxLength(15)]
         public string? VehiculoApellido { get; set; }
 
-        [Display(Name = "Tipo de vehiculo")]
+        [Display(Name = "Tipo de carroceria")]
         [Required(ErrorMessage = "Debe ingresar el tipo de vehiculo")]
         [MinLength(3, ErrorMessage = "El nombre ingresado debe poseer mas de tres letras")]
         [MaxLength(15)]
@@ -34,8 +34,7 @@ namespace Proyect_RaceTrack.ViewModels.VehiculoViewModels
 
         [Display(Name = "Matricula")]
         [Required(ErrorMessage = "Debe ingresar el numero de matricula")]
-        // [MinLength(3, ErrorMessage = "El nombre ingresado debe poseer mas de tres letras")]  
-        // [MaxLength(15)] 
+
         [MinLength(6, ErrorMessage = "La matricula debe contener al menos 6 caracteres")]
         [MaxLength(7)]
         public string? VehiculoMatricula { get; set; }
