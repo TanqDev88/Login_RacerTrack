807af71271e571e7805eafcdbf54099483385459
tanqueta88 <oliverprim783@gmail.com>
2024-02-14 02:29:43 -0300

    RoleDelete view is added

---
+++ Proyect_RaceTrack/ViewModels/RoleViewModels/RoleDeleteViewModel.cs
@@ -0,0 +1,8 @@
+namespace Proyect_RaceTrack.ViewModels
+{
+    public class RoleDeleteViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
