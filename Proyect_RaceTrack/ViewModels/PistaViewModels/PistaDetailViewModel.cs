e8fe553d49b41f0e004c7a097c394aac9427fa5e
tanqueta88 <oliverprim783@gmail.com>
2024-02-16 00:12:30 -0300

    Reorganization of the PistaViewModels folder.

--- Proyect_RaceTrack/ViewModels/PistaViewModels/PistaDetailViewModel.cs
+++ Proyect_RaceTrack/ViewModels/PistaViewModels/PistaDetailViewModel.cs
@@ -1,22 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Proyect_RaceTrack.Models;
-using System.ComponentModel.DataAnnotations;
-using Proyect_RaceTrack.Utils;
-
-namespace Proyect_RaceTrack.ViewModels.PistaViewModels;
-
-public class PistaDetailViewModel{
-
-        public int PistaId { get; set; }
-        [Display(Name = "Nombre")]
-        public string? PistaNombre {get;set;}
-        [Display(Name = "Pista codigo")]
-        public int PistaCodigo { get; set; }
-        public PistaType PistaMaterial { get; set; }
-        [Display(Name = "Iluminacion")]using Proyect_RaceTrack.Models;
 using System.ComponentModel.DataAnnotations;
 using Proyect_RaceTrack.Utils;
 
@@ -40,14 +22,4 @@ public class PistaDetailViewModel{
         public int CocheraId {get; set;}
         public List<Cochera>? Cocheras {get;set;}
 
-}
-
-        public bool PistaIluminacion {get;set;} = true;
-        [Display(Name = "Aprovisionamiento")]
-        public bool PistaAprovisionamiento {get;set;} = true;
-
-        public int CocheraId {get; set;}
-        //        [Display(Name = "Tipo de aeronave")]
-        public List<Cochera>? Cocheras {get;set;}
-        //public string? CocheraNombre { get; set; }
 }
