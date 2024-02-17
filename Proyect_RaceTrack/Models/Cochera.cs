88ed27e01963479db694c0282b043844171e30ed
tanqueta88 <oliverprim783@gmail.com>
2024-02-17 03:24:59 -0300

    Cochera model is modified

--- Proyect_RaceTrack/Models/Cochera.cs
+++ Proyect_RaceTrack/Models/Cochera.cs
@@ -2,20 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyect_RaceTrack.Models
 {
     public class Cochera
     {
-        // public List<Cochera> cocheras { get; set; } = new List<Cochera>();
-        // public String? NameFilterCoc { get; set; }
         public int CocheraId { get; set; }
+        [Display(Name = "Nombre")]
         public string? CocheraNombre { get; set; }
+        [Display(Name = "Numero")]
         public int CocheraNumero { get; set; }
+        [Display(Name = "Sector")]
         public CocheraType CocheraSector { get; set; }
+        [Display(Name = "Mantenimiento")]
         public bool CocheraAptoMantenimiento { get; set; } = true;
+        [Display(Name = "Oficinas")]
         public bool CocheraOficinas { get; set; } = true;
-        public virtual List<Pista> Pistas { get; set; }
+        public virtual List<Pista>? Pistas { get; set; }
         
     }
 }
