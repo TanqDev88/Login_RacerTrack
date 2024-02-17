61b71119d35b5d50871c6d91db80b447f23d8147
tanqueta88 <oliverprim783@gmail.com>
2024-02-17 03:24:18 -0300

    Calculadora model is modified

---
+++ Proyect_RaceTrack/Models/Calculadora.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyect_RaceTrack.Models
+{
+    public class Calculadora
+    {
+        public int Id { get; set; }
+        public int Costo { get; set; }
+        public int Cantidad { get; set; }
+        public int Resultado { get; set; }
+    }
+}
