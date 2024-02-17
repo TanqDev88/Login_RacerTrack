0407010e3f37294e8e47e9fedc80841e426f099a
tanqueta88 <oliverprim783@gmail.com>
2024-02-17 03:25:27 -0300

    Pista model is modified

--- Proyect_RaceTrack/Models/Pista.cs
+++ Proyect_RaceTrack/Models/Pista.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Proyect_RaceTrack.Utils;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyect_RaceTrack.Models
 {
     public class Pista
     {
         public int PistaId { get; set; }
+        [Display(Name = "Nombre")]
         public string? PistaNombre { get; set; }
+        [Display(Name = "Codigo")]
         public int PistaCodigo { get; set; }
+        [Display(Name = "Material")]
         public PistaType PistaMaterial { get; set; }
+        [Display(Name = "Iluminacion")]
         public bool PistaIluminacion { get; set; } = true;
+        [Display(Name = "Aprovisionamiento")]
         public bool PistaAprovisionamiento { get; set; } = true;
         public virtual List<Cochera> Cocheras { get; set; }
         
